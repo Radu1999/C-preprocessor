017bd505cb4ca4811fcb1a5ab1ddc011d3b04f55
Radu1999 <rchivereanu@gmail.com>
2022-03-14 23:49:41 +0200

    simple if else directives

--- main.c
+++ main.c
@@ -23,6 +23,16 @@ int compare_function_strings(void *a, void *b)
     return strcmp((char *)a, (char *)b);
 }
 
+int evaluate_string(char *str)
+{
+    return strlen(str) > 0 && str[0] != '0';
+}
+
+int if_ended_or_switched(char *word)
+{
+    return !strcmp(word, "#elif") || !strcmp(word, "#else") || !strcmp(word, "#endif");
+}
+
 char *solve_defines(FILE *in, HashTable *ht, char *line)
 {
     char delim[26] = "\t \n[]{}<>=+-*/%!&|^.,:;()\\";
@@ -31,7 +41,11 @@ char *solve_defines(FILE *in, HashTable *ht, char *line)
     char key[100];
     char value[100];
 
+    int in_if = 0;
+    char condition[100];
     int offset;
+    int is_true = 0;
+
     while (fgets(line, LINE_SIZE, in))
     {
         offset = 0;
@@ -41,44 +55,81 @@ char *solve_defines(FILE *in, HashTable *ht, char *line)
         char *token;
         while ((token = strsep(&ptr, delim)) != NULL)
         {
+            if (in_if == 2 && !is_true && !if_ended_or_switched(token))
+            {
+                break;
+            }
+            if (!strcmp(token, "#else"))
+            {
+                is_true = !is_true;
+            }
+            else if (!strcmp(token, "#if"))
+            {
+                in_if = 1;
+            }
+            else if (in_if == 1)
+            {
+                memcpy(condition, line + offset, strlen(line) - offset - 1);
+                condition[strlen(line) - offset - 1] = '\0';
 
-            if (!strcmp(token, "#define"))
-            {
-                completed_def = 1;
-            }
-            else if (completed_def == 1)
-            {
-                memcpy(key, token, strlen(token) + 1);
-                completed_def = 2;
-            }
-            else if (completed_def == 2)
-            {
-                //[TO DO] I can also have an expression
-                memcpy(value, line + offset, strlen(line) - offset - 1);
-                put(ht, key, strlen(key), value, strlen(line) - offset - 1);
-                completed_def = 0;
-                break;
-            }
-            else if (!strcmp("#undef", token))
-            {
-                completed_udef = 1;
-            }
-            else if (completed_udef == 1)
-            {
-                remove_ht_entry(ht, token);
-                completed_udef = 0;
-                break;
-            }
-            else
-            {
-                int initial_len = strlen(token);
-                while (has_key(ht, token))
+                char *value = condition;
+                while (has_key(ht, value))
                 {
-                    token = get(ht, token);
+                    value = get(ht, value);
                 }
-                printf("%s", token);
-                printf("%.*s", (int)(ptr - prev - initial_len), line + offset + initial_len);
+
+                is_true = evaluate_string(value);
+                in_if = 2;
+                break;
             }
+            else if (!strcmp(token, "#endif"))
+            {
+                in_if = 0;
+            }
+            else if ((in_if == 2 && is_true) || in_if == 0)
+
+            {
+                if (!strcmp(token, "#define"))
+                {
+                    completed_def = 1;
+                }
+                else if (completed_def == 1)
+                {
+                    memcpy(key, token, strlen(token) + 1);
+                    key[strlen(token) + 1] = '\0';
+                    completed_def = 2;
+                }
+                else if (completed_def == 2)
+                {
+                    memcpy(value, line + offset, strlen(line) - offset - 1);
+                    value[strlen(line) - offset - 1] = '\0';
+
+                    put(ht, key, strlen(key), value, strlen(line) - offset - 1);
+                    completed_def = 0;
+                    break;
+                }
+                else if (!strcmp("#undef", token))
+                {
+                    completed_udef = 1;
+                }
+                else if (completed_udef == 1)
+                {
+                    remove_ht_entry(ht, token);
+                    completed_udef = 0;
+                    break;
+                }
+                else
+                {
+                    int initial_len = strlen(token);
+                    while (has_key(ht, token))
+                    {
+                        token = get(ht, token);
+                    }
+                    printf("%s", token);
+                    printf("%.*s", (int)(ptr - prev - initial_len), line + offset + initial_len);
+                }
+            }
+
             offset += (int)(ptr - prev);
             prev = ptr;
         }
